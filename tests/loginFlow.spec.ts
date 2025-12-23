2db45cd8c7983ec4b6ba0dae19c02498f0465958
yashwanth2692 <105297630+yashwanth2692@users.noreply.github.com>
2025-12-23 22:29:35 +0530

    Refactor login flow tests to use a constant URL and improve locator consistency

--- tests/loginFlow.spec.ts
+++ tests/loginFlow.spec.ts
@@ -7,10 +7,12 @@ const password = 'admin123';
 const invalidUsername = 'InvalidUser';
 const invalidPassword = 'InvalidPass';
 
+const url = 'https://opensource-demo.orangehrmlive.com/';
+
 test('Positive Scenario Login Flow Orange HRM', async ({ page }) => {
 
     // Navigate to the Orange HRM Login Page
-    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.goto(url);
     // Verify the page title
     await expect (page).toHaveTitle('OrangeHRM');
 
@@ -19,16 +21,17 @@ test('Positive Scenario Login Flow Orange HRM', async ({ page }) => {
     // Enter Password
     await page.locator('//input[@placeholder="Password"]').fill(password);
     // Click on the Login Button
-    await page.locator('.oxd-button').click();
+    await page.locator('//button[@type="submit"]').click();
 
     // Verify successful login by checking the presence of the Dashboard
     await expect(page.locator('//h6[text()="Dashboard"]')).toBeVisible();
+    await expect(page.locator('//div[@class="oxd-topbar-header"]')).toHaveText('Dashboard Upgradeyog user');
 })
 
 test('Negative Scenario Login Flow Orange HRM', async ({ page }) => {
 
     // NAvigate to the Orange HRM Login Page
-    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.goto(url);
 
     // Verify the page title
     await expect (page).toHaveTitle('OrangeHRM');
@@ -41,4 +44,6 @@ test('Negative Scenario Login Flow Orange HRM', async ({ page }) => {
     await page.locator('.oxd-button').click();
     // Verify error message for invalid login
     await expect (page.locator('//div//div//i/following-sibling::p')).toHaveText('Invalid credentials');
+    // Staying on the Login Page
+    await expect (page).toHaveTitle('OrangeHRM');
 })
